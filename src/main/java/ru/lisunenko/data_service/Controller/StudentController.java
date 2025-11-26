9ffb9c3aa352a9690a1f5d1e674ccc3d35dd73de
Manhyato <145689933+Manhyato@users.noreply.github.com>
2025-11-26 13:28:01 +0300

    change openapi-data and recompile project. Fix StudentController

--- src/main/java/ru/lisunenko/data_service/Controller/StudentController.java
+++ src/main/java/ru/lisunenko/data_service/Controller/StudentController.java
@@ -3,28 +3,27 @@ package ru.lisunenko.data_service.Controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import ru.lisunenko.data.api.StudentApi;
+import ru.lisunenko.data.api.StudentDataApi;
 import ru.lisunenko.data.model.StudentDataCreateRequest;
 import ru.lisunenko.data.model.StudentDataResponse;
-import ru.lisunenko.data_service.service.StudentRepository;
 import ru.lisunenko.data_service.service.Student;
-
+import ru.lisunenko.data_service.service.StudentRepository;
 
 @RestController
 @RequiredArgsConstructor
-public class StudentController implements StudentApi {
+public class StudentController implements StudentDataApi {
 
     private final StudentRepository studentRepository;
 
     @Override
-    public ResponseEntity<StudentDataResponse> studentsPost(StudentDataCreateRequest request) {
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
         Student student = new Student();
         student.setName(request.getFullName());
         student.setPassport(request.getPassport());
         studentRepository.save(student);
 
         StudentDataResponse response = new StudentDataResponse();
-        response.setId(student.getId().intValue());
+        response.setId(student.getId());
         response.setFullName(student.getName());
         response.setPassport(student.getPassport());
 
@@ -32,16 +31,17 @@ public class StudentController implements StudentApi {
     }
 
     @Override
-    public ResponseEntity<StudentDataResponse> studentsGet(Integer id) {
-        Student student = studentRepository.findById(Long.valueOf(id)).orElse(null);
+    public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+        Student student = studentRepository.findById(id).orElse(null);
         if (student == null) return ResponseEntity.notFound().build();
 
         StudentDataResponse response = new StudentDataResponse();
-        response.setId(student.getId().intValue());
+        response.setId(student.getId());
         response.setFullName(student.getName());
         response.setPassport(student.getPassport());
 
         return ResponseEntity.ok(response);
     }
 }
+
 
